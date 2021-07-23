23e71f0d04e16f1339f3de3c8ee5af2c8fafaa29
chenw <1105013796@qq.com>
2021-07-23 22:46:03 +0800

    v1.0.0-update：poi导出补充

--- poi-export/src/main/java/ink/cwblog/poiexport/util/ExcelUtil.java
+++ poi-export/src/main/java/ink/cwblog/poiexport/util/ExcelUtil.java
@@ -110,6 +110,52 @@ public class ExcelUtil {
         return null;
     }
 
+    public static Boolean createSheet(Sheet sheet,@Valid @NotEmpty(message = "excel数据不能为空") List data){
+        try {
+            //创建表格
+
+            Row headRow = sheet.createRow(0);
+            String[] fieldsNames = getFieldsName(data.get(0).getClass());
+            //设置表头
+            for (int i = 0;i< fieldsNames.length;i++){
+                headRow.createCell(i).setCellValue(fieldsNames[i]);
+            }
+            //填充数据
+            Method [] methods = new Method[fieldsNames.length];
+            for (int i = 0; i<data.size();i++){
+                Row row = sheet.createRow(i+1);
+                Object obj = data.get(i);
+                for (int j = 0; j < fieldsNames.length; j++) {
+                    //加载第一行数据时，初始化所有属性的getter方法
+                    if(i == 0){
+                        String fieldName = fieldsNames[j];
+                        //处理布尔值命名 "isXxx" -> "setXxx"
+                        if (fieldName.contains("is")) {
+                            fieldName = fieldName.split("is")[1];
+                        }
+                        methods[j] = obj.getClass().getMethod("get" +
+                                fieldName.substring(0,1).toUpperCase() +
+                                fieldName.substring(1));
+                    }
+                    Cell cell = row.createCell(j);
+                    Object value = methods[j].invoke(obj);
+                    //注意判断 value 值是否为空
+                    if(value == null){
+                        value = "无";
+                    }
+                    cell.setCellValue(value.toString());
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
     /**
      * 根据对象获取其属性名
      * @param clazz
